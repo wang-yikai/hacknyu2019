9080b93f2e302c9db03506aa221d7477054417e7
Yikai <calvin0987@gmail.com>
2019-02-17 02:45:22 -0500

    trend done

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, session, url_for, redirect
 import json
 import datetime
+from copy import deepcopy
 from sqlite3 import connect
 from hashlib import sha1
 from semantics3 import Products
@@ -10,6 +11,20 @@ sem3 = Products(
 	api_secret = "YmVkZGQ1YWY1NTQxOThlZTE5Y2FlMDU1Nzc2MTlmMDQ"
 )
 f = "data/database.db"
+
+def get_median(l):
+	l = sorted(l)
+	if len(l) % 2 == 1:
+		return l[len(l)//2]
+	return (l[len(l)//2] + l[len(l)//2 - 1])/2
+
+def insert_interval(l, interval):
+	for i in range(len(l)):
+		if interval[1] <= l[i][1]:
+			l.insert(i, interval)
+			return l
+	l.append(interval)
+	return l
 
 def to_datetime(d):
     return datetime.datetime.fromtimestamp(
@@ -136,6 +151,55 @@ def remove_from_watchlist(user, item):
 	db.commit()
 	db.close()
 
+def trend_median(price_history):
+	if len(price_history) < 1:
+		# print("here")
+		return []
+
+	trend = []
+	curr_prices = [ price_history[0][-1] ]
+	curr_intervals = [ price_history[0] ]
+	last_start = price_history[0][0]
+	for i in range(1, len(price_history)):
+		# print(i)
+		j = 0
+		while j < len(curr_intervals):
+			# |------|
+					  # |---------|
+			# |-------|
+					# |-------|
+			if curr_intervals[j][1] <= price_history[i][0]:
+				# print("1")
+				trend.append([ last_start, curr_intervals[j][1], get_median(curr_prices) ])
+				last_start = price_history[i][0]
+				curr_prices.remove(curr_intervals[j][-1])
+				curr_intervals.pop(j)
+
+			# |--------|
+			# 	|------|
+			else:
+				# print("2")
+				trend.append([ last_start, price_history[i][0], get_median(curr_prices)])
+				last_start = price_history[i][0]
+				curr_prices.append(price_history[i][-1])
+				j = len(curr_intervals)
+
+		# print("3")
+		curr_intervals = insert_interval(curr_intervals, price_history[i])
+
+		# |-----------|
+		# 	|------|
+	while len(curr_intervals) > 1:
+		trend.append([last_start, curr_intervals[1][1], get_median(curr_prices)])
+		last_start = curr_intervals[1][1]
+		curr_prices.remove(curr_intervals[0][-1])
+		curr_intervals.pop(0)
+
+	trend.append([ last_start, curr_intervals[0][1], get_median(curr_prices) ])
+	return sorted(trend)
+
+
+
 
 sem3.offers_field("sem3_id", "7JAykYaFyiYscEmcAwYC64")
 
@@ -145,28 +209,31 @@ price_history = []
 print("_______Price History______")
 # print(offers['results'])
 for elem in offers['results']:
-    price_history.append([ to_datetime(elem['lastrecorded_at']), to_datetime(elem['firstrecorded_at']), elem['price'] ])
+    price_history.append([ int(elem['firstrecorded_at']), int(elem['lastrecorded_at']), float(elem['price']) ])
 
-sorted(price_history)
+price_history = sorted(price_history)
 print(price_history)
 
-sem3.products_field("search", "iPhone")
+# sem3.products_field("search", "iPhone")
+#
+# results = sem3.get_products()
 
-results = sem3.get_products()
+# print("_______Product Search List_____")
+# # print(results)
+# for elem in results['results']:
+#     print("name:", elem['name'], "sem3_id:", elem['sem3_id'])
 
-print("_______Product Search List_____")
-# print(results)
-for elem in results['results']:
-    print("name:", elem['name'], "sem3_id:", elem['sem3_id'])
+print("_____TREND____")
+print(trend_median(price_history))
 
-print("1:",register("bob", "thebuilder"))
-print("2:",login("bob", "thebuilder"))
-print("3:",login("what", "haha"))
-print("4:",register("what", "haha"))
-print("5:",register("bob", "thebuilder"))
-print("6:",add_to_watchlist("bob", "iPhone"))
-print("7:",add_to_watchlist("bob", "PC"))
-print("8:",get_watchlist("bob"))
-print("9:",add_to_watchlist("bob", "iPhone"))
-print("end:",remove_from_watchlist("bob", "iPhone"))
-print("10:",get_watchlist("bob"))
+# print("1:",register("bob", "thebuilder"))
+# print("2:",login("bob", "thebuilder"))
+# print("3:",login("what", "haha"))
+# print("4:",register("what", "haha"))
+# print("5:",register("bob", "thebuilder"))
+# print("6:",add_to_watchlist("bob", "iPhone"))
+# print("7:",add_to_watchlist("bob", "PC"))
+# print("8:",get_watchlist("bob"))
+# print("9:",add_to_watchlist("bob", "iPhone"))
+# print("end:",remove_from_watchlist("bob", "iPhone"))
+# print("10:",get_watchlist("bob"))
